6df0f24d8989f8ecf178177f80337ae53347b794
Carlos Cobo <toqueteos@gmail.com>
2013-04-03 03:09:56 +0200

    nbtdebug: Use new proto/nbt features.

--- bin/nbtdebug/main.go
+++ bin/nbtdebug/main.go
@@ -2,47 +2,76 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+	"testing/iotest"
 
 	"github.com/toqueteos/minero/proto/nbt"
 )
 
-const (
-	// Uncompressed
-	Servers = `C:\Users\Carlos\AppData\Roaming\.minecraft\servers.dat`
-	// Gzip
-	BigTest = "../_testdata/bigtest.nbt"
-	Test    = "../_testdata/test.nbt"
-)
+type Flags struct {
+	Quiet bool
+}
+
+var flags = new(Flags)
 
 func usage() {
-	fmt.Println("Usage: nbtdebug nbtfile")
+	os.Stderr.WriteString("Usage: nbtdebug nbtfile")
+}
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("nbtdebug> ")
+	flag.BoolVar(&flags.Quiet, "q", false, "")
+	flag.Parse()
 }
 
 func main() {
-	flag.Parse()
-
 	if flag.NArg() != 1 {
 		usage()
 		return
 	}
 
-	var filename = flag.Arg(0)
+	var (
+		filename = flag.Arg(0)
+		err      error
+	)
+
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("Couldn't open file: %q.\n", filename)
 	}
 
-	c, err := nbt.Read(file)
+	var (
+		r, rr io.Reader
+		gzip  bool
+	)
+
+	if !flags.Quiet {
+		r = iotest.NewReadLogger("nbtdebug:", file)
+	} else {
+		r = file
+	}
+
+	rr, gzip, err = nbt.GuessCompression(r)
+	if err != nil {
+		log.Fatalf("Error guessing file compression: %q\n", err)
+	}
+
+	if gzip {
+		log.Println("Detected gzip file.")
+	}
+
+	c, err := nbt.ReadRaw(rr)
 	if err != nil {
 		log.Fatalln("nbt.Read:", err)
 	}
 
-	for k, v := range c.Tags {
+	log.Println("Top level compound name:", c.Name)
+	for k, v := range c.Value {
 		parts := strings.SplitN(k, " ", 2)
-		fmt.Printf("%q: %v\n", parts[0], v)
+		log.Printf("%q: %v\n", parts[0], v)
 	}
 }
