004cf2b95c901cd6cb377ae21ac86ddd40a929a7
Carlos Cobo <toqueteos@gmail.com>
2013-04-13 20:44:43 +0200

    proto/auth: Fix SHA-1 AuthDigest tests.

--- proto/auth/online_mode_test.go
+++ proto/auth/online_mode_test.go
@@ -2,6 +2,10 @@
 package auth
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"strings"
 	"testing"
 )
 
@@ -10,9 +14,9 @@ import (
 var tests = []struct {
 	in, out string
 }{
-	{"Notch": "4ed1f46bbe04bc756bcb17c0c7ce3e4632f06a48"},
-	{"jeb_": "-7c9d5b0044c130109a5d7b5fb5c317c02b4e28c1"},
-	{"simon": "88e16a1019277b15d58faf0541e11910eb756f6"},
+	{"Notch", "4ed1f46bbe04bc756bcb17c0c7ce3e4632f06a48"},
+	{"jeb_", "-7c9d5b0044c130109a5d7b5fb5c317c02b4e28c1"},
+	{"simon", "88e16a1019277b15d58faf0541e11910eb756f6"},
 }
 
 // authDigest is AuthDigest's testing version. It's here just to ensure
