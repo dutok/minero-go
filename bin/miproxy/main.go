a691b886630ff66de5d7aa7b391265f494ab9a98
Carlos Cobo <toqueteos@gmail.com>
2013-04-14 14:30:52 +0200

    bin/miproxy: Little refactor.
    - Reduce var pollution: Flags, flags, flag -> Flags, flag.

--- bin/miproxy/main.go
+++ bin/miproxy/main.go
@@ -10,66 +10,67 @@ import (
 	"os"
 )
 
-type Flags struct {
+var Flags struct {
 	Src, Dst, File string
 	Record, Gzip   bool
 }
 
-// Value of each flag
-var f Flags
-
 // Usage of each flag
 var u = map[string]string{
-	"src":    "Address where proxy will read from.",
-	"dst":    "Address where proxy will listen for.",
-	"file":   "File to store proxy stream.",
-	"record": "Save proxy stream to storage?",
-	"gzip":   "Compress with gzip storage?",
+	"src":    "Proxy destination address.",
+	"dst":    "Proxy listen address.",
+	"file":   "Stream storage file.",
+	"record": "Save stream to storage?",
+	"gzip":   "Compress stream with gzip before storing?",
 }
 
 func init() {
-	flag.StringVar(&f.Src, "src", "127.0.0.1:25565", u["src"])
-	flag.StringVar(&f.Dst, "dst", "127.0.0.1:26665", u["dst"])
-	flag.StringVar(&f.File, "file", "proxy.log", u["file"])
-	flag.BoolVar(&f.Record, "record", true, u["record"])
-	flag.BoolVar(&f.Gzip, "gzip", true, u["gzip"])
+	flag.StringVar(&Flags.Src, "src", "127.0.0.1:25565", u["src"])
+	flag.StringVar(&Flags.Dst, "dst", "127.0.0.1:26665", u["dst"])
+	flag.StringVar(&Flags.File, "file", "proxy.log", u["file"])
+	flag.BoolVar(&Flags.Record, "record", true, u["record"])
+	flag.BoolVar(&Flags.Gzip, "gzip", true, u["gzip"])
 
 	flag.Parse()
 }
 
 func main() {
+	log.SetPrefix("miproxy> ")
+	log.SetFlags(log.Ltime)
+
 	var err error
 
-	fmt.Print("Is remote server online? ... ")
-	serverConn, err := net.Dial("tcp", f.Src)
+	log.Println("Pinging remote server.")
+	serverConn, err := net.Dial("tcp", Flags.Src)
 	if err != nil {
+		log.Println("Remote server: OFFLINE.")
 		log.Fatalln(err)
 	}
 	defer serverConn.Close()
-	fmt.Println("OK")
+	log.Println("Remote server: ONLINE.")
 
-	fmt.Print("Setting up proxy ... ")
-	listener, err := net.Listen("tcp", f.Dst)
+	log.Println("Setting up proxy.")
+	listener, err := net.Listen("tcp", Flags.Dst)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer listener.Close()
-	fmt.Println("OK")
-	fmt.Printf("Listening on %q. OK\n", f.Dst)
+	log.Println("Done!")
+	log.Printf("Listening on %q\n", Flags.Dst)
 
 	var file io.Writer
 
 	// Log proxy stream
-	if f.Record {
+	if Flags.Record {
 		var err error
 
 		fileFlags := os.O_CREATE | os.O_TRUNC | os.O_WRONLY
-		file, err = os.OpenFile(f.File, fileFlags, 0666)
+		file, err = os.OpenFile(Flags.File, fileFlags, 0666)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		if f.Gzip {
+		if Flags.Gzip {
 			file = gzip.NewWriter(file)
 		}
 	}
