07d872c391d2615c78b5492389c3bd91b0b1c160
Carlos Cobo <toqueteos@gmail.com>
2013-04-17 12:36:06 +0200

    bin/miproxy: Fix build errors.
    - Remove unused imports.
    - Duplicate `PrefixWriter` definition.

--- bin/miproxy/main.go
+++ bin/miproxy/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"compress/gzip"
 	"flag"
-	"fmt"
 	"io"
 	"log"
 	"net"
@@ -98,32 +97,4 @@ func main() {
 		// Client <- Server
 		go io.Copy(clientConn, server)
 	}
-}
-
-type PrefixWriter struct {
-	rw     io.Writer
-	Prefix string
-}
-
-func (pw *PrefixWriter) Write(p []byte) (n int, err error) {
-	var nn int
-	prefix := []byte(pw.Prefix + " ")
-
-	// Write prefix
-	n, err = pw.rw.Write(prefix)
-	if err != nil {
-		return
-	}
-	nn += n
-
-	// Write content
-	n, err = pw.rw.Write(p)
-	if err != nil {
-		// Prefix write succesful
-		n = nn
-		return
-	}
-	nn += n
-
-	return nn, nil
 }
