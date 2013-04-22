eb166f39d7bd2c5317da6edf573fa5b38db0b6bf
Carlos Cobo <toqueteos@gmail.com>
2013-04-22 02:37:58 +0200

    Docs. Hints for godoc.org viewers.

--- minero.go
+++ minero.go
@@ -1,4 +1,7 @@
 // Defines important constants such as: latest NMS supported version.
+//
+// If you are viewing this from godoc.org/github.com/toqueteos/minero just
+// scroll down a bit to see the full list of packages.
 package minero
 
 const (
