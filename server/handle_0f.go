50b0b802eb42af029bece726a49d27e448bcc864
Carlos Cobo <toqueteos@gmail.com>
2013-04-28 00:51:59 +0200

    server: Basic BlockPlace support in 0x0F handler.

--- server/handle_0f.go
+++ server/handle_0f.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 
+	"github.com/toqueteos/minero/material"
 	"github.com/toqueteos/minero/proto/packet"
 	"github.com/toqueteos/minero/server/player"
 )
@@ -13,4 +14,21 @@ func Handle0F(server *Server, sender *player.Player) {
 	pkt.ReadFrom(sender.Conn)
 
 	log.Printf("PlayerBlockPlace: %+v", pkt)
+
+	var bid int16
+	if pkt.HeldItem != nil {
+		bid = pkt.HeldItem.BlockId
+		if pkt.HeldItem.BlockId == -1 {
+			bid = int16(material.Stone)
+		}
+	}
+
+	r := &packet.BlockChange{
+		X:         pkt.X, // int32
+		Y:         pkt.Y, // int8
+		Z:         pkt.Z, // int32
+		BlockType: bid,   // int16 // New block type for block
+		BlockMeta: 0,     // int8
+	}
+	server.BroadcastPacket(r)
 }
