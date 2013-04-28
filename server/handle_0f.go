3391109ec1cb8a508485afd6307c098b8db88141
Carlos Cobo <toqueteos@gmail.com>
2013-04-28 19:26:47 +0200

    server: 0x0F related. Handle metadata.

--- server/handle_0f.go
+++ server/handle_0f.go
@@ -15,20 +15,21 @@ func Handle0F(server *Server, sender *player.Player) {
 
 	log.Printf("PlayerBlockPlace: %+v", pkt)
 
-	var bid int16
+	var bid, meta int16
 	if pkt.HeldItem != nil {
 		bid = pkt.HeldItem.BlockId
+		meta = pkt.HeldItem.Damage
 		if pkt.HeldItem.BlockId == -1 {
 			bid = int16(material.Stone)
 		}
 	}
 
 	r := &packet.BlockChange{
-		X:         pkt.X, // int32
-		Y:         pkt.Y, // int8
-		Z:         pkt.Z, // int32
-		BlockType: bid,   // int16 // New block type for block
-		BlockMeta: 0,     // int8
+		X:         pkt.X,      // int32
+		Y:         pkt.Y,      // int8
+		Z:         pkt.Z,      // int32
+		BlockType: bid,        // int16 // New block type for block
+		BlockMeta: int8(meta), // int8
 	}
 	server.BroadcastPacket(r)
 }
