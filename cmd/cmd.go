0aa35a99e2516816c14a887750e7fda0aaadbfe4
Carlos Cobo <toqueteos@gmail.com>
2013-04-29 18:55:57 +0200

    Pkg name refactor: command -> cmd.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,5 +1,5 @@
-// Package command provides an easily extendable interface to work with chat commands.
-package command
+// Package cmd provides an easily extendable interface to work with chat commands.
+package cmd
 
 import (
 	"github.com/toqueteos/minero/server/player"
